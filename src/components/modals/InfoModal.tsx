02350bacbbb23ec682970e6cb28e28b94454f741
Arian Jamasb <arjamasb@gmail.com>
2022-01-15 23:24:37 +0000

    typo

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -57,7 +57,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
                     <p className="text-sm text-gray-500">
                       Guess the PEPTIRDLE in 6 tries. After each guess, the color
                       of the tiles will change to show how close your guess was
-                      to the word.
+                      to the peptide.
                     </p>
 
                     <div className="flex justify-center mb-1 mt-4">
